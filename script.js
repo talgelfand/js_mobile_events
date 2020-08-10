a620c9a93282a5d049c5b1cca271cb28ba450148
Tal <talj.gelfand@gmail.com>
2020-08-10 20:10:35 +0300

    События на мобильных устройствах

---
+++ script.js
@@ -0,0 +1,33 @@
+// touchstart
+// touchmove
+// touchend
+// touchenter
+// touchleave
+// touchcancel
+
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchstart', (event) => {
+        event.preventDefault();
+
+        console.log('Start');
+        console.log(event.targetTouches);
+    });
+
+    box.addEventListener('touchmove', (event) => {
+        event.preventDefault();
+
+        console.log(event.targetTouches[0].pageX);
+    });
+
+    // box.addEventListener('touchend', (event) => {
+    //     event.preventDefault();
+
+    //     console.log('End');
+    // });
+});
+
+// touches
+// targetTouches
+// changedTouches
